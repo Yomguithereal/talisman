502a4bf18c07db0bb0d279f377ca3aa3799de3bb
Yomguithereal <guillaumeplique@gmail.com>
2016-03-27 14:10:38 +0200

    Starting tfidf

---
+++ src/stats/tfidf.js
@@ -0,0 +1,34 @@
+/**
+ * Talisman stats/tfidf
+ * =====================
+ *
+ * Helpers related to TF/IDF computations.
+ */
+import words from '../tokenizers/words/naive';
+
+// TODO: do we need a name for the document
+// TODO: create the handy aliases functions
+
+/**
+ * TfIdf class
+ *
+ * @constructor
+ * @param {array} documents - Initial documents.
+ */
+export class TfIdf {
+  constructor(documents) {
+    documents = documents || [];
+
+    // Properties
+  }
+
+  /**
+   * Adding a document to the computation.
+   *
+   * @param  {mixed} document - The document to add.
+   * @return {TfIdf}          - Returns itself for chaining purposes.
+   */
+  addDocument(document) {
+
+  }
+}
