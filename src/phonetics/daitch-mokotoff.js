7caf2efffee5d3f61bd08bbc4e3e339ca3b4b1c4
Yomguithereal <guillaumeplique@gmail.com>
2016-04-11 16:48:26 +0200

    Improved daitch-mokotoff perfs

--- src/phonetics/daitch-mokotoff.js
+++ src/phonetics/daitch-mokotoff.js
@@ -14,66 +14,130 @@ import deburr from 'lodash/deburr';
  *
  * [Note]:
  * For the (RS|RZ) part, the original algo says (94, 4) but most
- * implementations drop it to only (94).
+ * implementations drop it to only (94). This implementation follows the
+ * original algo.
  */
+const MNNM = /^(MN|NM)/,
+      MN = /^(M|N)/;
 
-// TODO: optimize to cut O(n * m)
-const RULES = [
-  [/^(AI|AJ|AY)/, 0, 1, null],
-  [/^AU/, 0, 7, null],
-  [/^Ą/, null, null, [6, null]],
-  [/^A/, 0, null, null],
-  [/^B/, 7, 7, 7],
-  [/^CHS/, 5, 54, 54],
-  [/^CH/, [5, 4], [5, 4], [5, 4]],
-  [/^CK/, [5, 45], [5, 45], [5, 45]],
-  [/^(CSZ|CZS|CZ|CS)/, 4, 4, 4],
-  [/^C/, [5, 4], [5, 4], [5, 4]],
-  [/^(DRZ|DRS|DSH|DSZ|DZH|DZS|DS|DZ)/, 4, 4, 4],
-  [/^(DT|D)/, 3, 3, 3],
-  [/^(EI|EJ|EY)/, 0, 1, null],
-  [/^EU/, 1, 1, null],
-  [/^Ę/, null, null, [6, null]],
-  [/^E/, 0, null, null],
-  [/^(FB|F)/, 7, 7, 7],
-  [/^G/, 5, 5, 5],
-  [/^H/, 5, 5, null],
-  [/^(IA|IE|IO|IU)/, 1, null, null],
-  [/^I/, 0, null, null],
-  [/^J/, [1, 4], [null, 4], [null, 4]],
-  [/^KS/, 5, 54, 54],
-  [/^(KH|K)/, 5, 5, 5],
-  [/^L/, 8, 8, 8],
-  [/^(MN|NM)/, null, 66, 66],
-  [/^(M|N)/, 6, 6, 6],
-  [/^(OI|OJ|OY)/, 0, 1, null],
-  [/^O/, 0, null, null],
-  [/^(PF|PH|P)/, 7, 7, 7],
-  [/^Q/, 5, 5, 5],
-  [/^(RZ|RS)/, [94, 4], [94, 4], [94, 4]],
-  [/^R/, 9, 9, 9],
-  [/^(SCHTSCH|SCHTSH|SCHTCH|SHTCH|SHCH|SHTSH)/, 2, 4, 4],
-  [/^SCH/, 4, 4, 4],
-  [/^(SHT|SCHT|SCHD)/, 2, 43, 43],
-  [/^SH/, 4, 4, 4],
-  [/^(STCH|STSCH|SC|STRZ|STRS|STSH)/, 2, 4, 4],
-  [/^ST/, 2, 43, 43],
-  [/^(SZCZ|SZCS)/, 2, 4, 4],
-  [/^(SZT|SHD|SZD|SD)/, 2, 43, 43],
-  [/^(SZ|S|TCH|TTCH|TTSCH)/, 4, 4, 4],
-  [/^TH/, 3, 3, 3],
-  [/^(TRZ|TRS|TSCH|TSH|TS|TTS|TTSZ|TC|TZ|TTZ|TZS|TSZ)/, 4, 4, 4],
-  [/^Ţ/, [3, 4], [3, 4], [3, 4]],
-  [/^T/, 3, 3, 3],
-  [/^(UI|UJ|UY)/, 0, 1, null],
-  [/^(U|UE)/, 0, null, null],
-  [/^(V|W)/, 7, 7, 7],
-  [/^X/, 5, 54, 54],
-  [/^Y/, 1, null, null],
-  [/^(ZDZ|ZDZH|ZHDZH)/, 2, 4, 4],
-  [/^(ZD|ZHD)/, 2, 43, 43],
-  [/^(ZH|ZS|ZSCH|ZSH|Z)/, 4, 4, 4]
-];
+const RULES = {
+  A: [
+    [/^(AI|AJ|AY)/, 0, 1, null],
+    [/^AU/, 0, 7, null],
+    [null, 0, null, null]
+  ],
+  Ą: [
+    [null, null, null, [6, null]]
+  ],
+  B: [
+    [null, 7, 7, 7]
+  ],
+  C: [
+    [/^CHS/, 5, 54, 54],
+    [/^CH/, [5, 4], [5, 4], [5, 4]],
+    [/^CK/, [5, 45], [5, 45], [5, 45]],
+    [/^(CSZ|CZS|CZ|CS)/, 4, 4, 4],
+    [null, [5, 4], [5, 4], [5, 4]]
+  ],
+  D: [
+    [/^(DRZ|DRS|DSH|DSZ|DZH|DZS|DS|DZ)/, 4, 4, 4],
+    [/^(DT|D)/, 3, 3, 3]
+  ],
+  E: [
+    [/^(EI|EJ|EY)/, 0, 1, null],
+    [/^EU/, 1, 1, null],
+    [null, 0, null, null],
+  ],
+  Ę: [
+    [null, null, null, [6, null]]
+  ],
+  F: [
+    [/^(FB|F)/, 7, 7, 7]
+  ],
+  G: [
+    [null, 5, 5, 5]
+  ],
+  H: [
+    [null, 5, 5, null]
+  ],
+  I: [
+    [/^(IA|IE|IO|IU)/, 1, null, null],
+    [null, 0, null, null]
+  ],
+  J: [
+    [null, [1, 4], [null, 4], [null, 4]]
+  ],
+  K: [
+    [/^KS/, 5, 54, 54],
+    [/^(KH|K)/, 5, 5, 5]
+  ],
+  L: [
+    [null, 8, 8, 8]
+  ],
+  M: [
+    [MNNM, null, 66, 66],
+    [MN, 6, 6, 6]
+  ],
+  N: [
+    [MNNM, null, 66, 66],
+    [MN, 6, 6, 6],
+  ],
+  O: [
+    [/^(OI|OJ|OY)/, 0, 1, null],
+    [null, 0, null, null]
+  ],
+  P: [
+    [/^(PF|PH|P)/, 7, 7, 7]
+  ],
+  Q: [
+    [null, 5, 5, 5]
+  ],
+  R: [
+    [/^(RZ|RS)/, [94, 4], [94, 4], [94, 4]],
+    [null, 9, 9, 9]
+  ],
+  S: [
+    [/^(SCHTSCH|SCHTSH|SCHTCH|SHTCH|SHCH|SHTSH)/, 2, 4, 4],
+    [/^SCH/, 4, 4, 4],
+    [/^(SHT|SCHT|SCHD)/, 2, 43, 43],
+    [/^SH/, 4, 4, 4],
+    [/^(STCH|STSCH|SC|STRZ|STRS|STSH)/, 2, 4, 4],
+    [/^ST/, 2, 43, 43],
+    [/^(SZCZ|SZCS)/, 2, 4, 4],
+    [/^(SZT|SHD|SZD|SD)/, 2, 43, 43],
+    [/^(SZ|S)/, 4, 4, 4]
+  ],
+  T: [
+    [/^(TCH|TTCH|TTSCH)/, 4, 4, 4],
+    [/^TH/, 3, 3, 3],
+    [/^(TRZ|TRS|TSCH|TSH|TS|TTS|TTSZ|TC|TZ|TTZ|TZS|TSZ)/, 4, 4, 4],
+    [null, 3, 3, 3]
+  ],
+  Ţ: [
+    [null, [3, 4], [3, 4], [3, 4]]
+  ],
+  U: [
+    [/^(UI|UJ|UY)/, 0, 1, null],
+    [/^(UE|U)/, 0, null, null]
+  ],
+  V: [
+    [null, 7, 7, 7]
+  ],
+  W: [
+    [null, 7, 7, 7]
+  ],
+  X: [
+    [null, 5, 54, 54]
+  ],
+  Y: [
+    [null, 1, null, null]
+  ],
+  Z: [
+    [/^(ZHDZH|ZDZH|ZDZ)/, 2, 4, 4],
+    [/^(ZHD|ZD)/, 2, 43, 43],
+    [/^(ZSCH|ZSH|ZH|ZS|Z)/, 4, 4, 4]
+  ],
+};
 
 /**
  * Helpers.
@@ -136,31 +200,38 @@ export default function daitchMokotoff(name) {
 
   // Applying the rules
   while (current.length) {
-    for (let i = 0, l = RULES.length; i < l; i++) {
+
+    // Find the subset of rules applying to the current letter
+    const firstLetter = current.charAt(0),
+          rules = RULES[firstLetter];
+
+    for (let i = 0, l = rules.length; i < l; i++) {
       const [
         pattern,
-        firstLetter,
+        ifFirstLetter,
         vowelNext,
         usual
-      ] = RULES[i];
+      ] = rules[i];
 
-      const match = current.match(pattern);
+      const match = pattern ? current.match(pattern) : [firstLetter];
 
       if (match) {
+        const offset = match[0].length;
 
         let correctCode = usual;
 
         if (start)
-          correctCode = firstLetter;
+          correctCode = ifFirstLetter;
 
-        else if (VOWELS.has(current.charAt(match[0].length)))
+        else if (VOWELS.has(current.charAt(offset)))
           correctCode = vowelNext;
 
         if (lastPattern !== pattern && correctCode !== null)
           code.push(correctCode);
 
-        lastPattern = pattern;
-        current = current.slice(match[0].length);
+        lastPattern = pattern || firstLetter;
+
+        current = current.slice(offset);
         break;
       }
     }
