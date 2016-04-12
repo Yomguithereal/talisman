1383cfc6ec423c17b79105afa28318dbf1c45d4b
Yomguithereal <guillaumeplique@gmail.com>
2016-04-12 13:22:54 +0200

    Preparing classification folder

---
+++ src/classification/abstract.js
@@ -0,0 +1,9 @@
+/**
+ * Talisman classification/abstract
+ * =================================
+ *
+ * Base class used as a template for other classifiers.
+ */
+export default class Classifier {
+
+}
