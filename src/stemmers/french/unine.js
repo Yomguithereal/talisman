a9aeb748d2c3171cd85b766a82684fac025c8a79
Yomguithereal <guillaumeplique@gmail.com>
2016-07-01 16:09:59 +0200

    Adding the complex version of UniNE French stemmer

--- src/stemmers/french/unine.js
+++ src/stemmers/french/unine.js
@@ -14,7 +14,47 @@
  * Savoy, J. (1999). A stemming procedure and stopword list for general French
  * corpora. Journal of the American Society for Information Science, 50(10),
  * 944-952.
+ *
+ * [Note]:
+ * It should be possible to fix some bug relevant to SOLR's implementation.
  */
+
+/**
+ * Function replacing the character at the given index in the target string.
+ *
+ * @param  {string} string - The target string.
+ * @param  {number} index  - Index of the character to substitute.
+ * @param  {string} char   - The replacing character.
+ * @return {string}        - The resulting string.
+ */
+function replaceAt(string, index, char) {
+  return string.substr(0, index) + char + string.substr(index + char.length);
+}
+
+/**
+ * Function deleting the character at the given index in the target string.
+ *
+ * @param  {string} string - The target string.
+ * @param  {number} index  - Index of the character to substitute.
+ * @return {string}        - The resulting string.
+ */
+function deleteAt(string, index) {
+  return string.substr(0, index) + string.substr(index + 1);
+}
+
+/**
+ * Function checking whether the string has the given suffix.
+ *
+ * @param  {string} string - The target string.
+ * @param  {number} length - Length offset.
+ * @param  {string} suffix - The considered suffix.
+ * @return {boolean}
+ */
+function endsWith(string, length, suffix) {
+  if (suffix.length > length)
+    return false;
+  return string.slice(0, length).slice(-suffix.length) === suffix;
+}
 
 /**
  * Function stemming the given world using the minimal UniNE algorithm for the
@@ -60,7 +100,234 @@ export default minimal;
  * @param  {string} word - The word to stem.
  * @return {string}      - The resulting stem.
  */
+function normalize(stem, length) {
+  if (length > 4) {
+    for (let i = 0; i < length; i++) {
+      switch (stem[i]) {
+        case 'à':
+        case 'á':
+        case 'â':
+          stem = replaceAt(stem, i, 'a');
+          break;
+        case 'ô':
+          stem = replaceAt(stem, i, 'o');
+          break;
+        case 'è':
+        case 'é':
+        case 'ê':
+          stem = replaceAt(stem, i, 'e');
+          break;
+        case 'ù':
+        case 'û':
+          stem = replaceAt(stem, i, 'u');
+          break;
+        case 'î':
+          stem = replaceAt(stem, i, 'i');
+          break;
+        case 'ç':
+          stem = replaceAt(stem, i, 'c');
+          break;
+        default:
+      }
+    }
+
+    let character = stem[0];
+
+    for (let i = 1; i < length; i++) {
+      if (stem[i] === character && /[^\W\d]/.test(character)) {
+        stem = deleteAt(stem, i--);
+        length--;
+      }
+      else {
+        character = stem[i];
+      }
+    }
+  }
+
+  if (length > 4 && endsWith(stem, length, 'ie'))
+    length -= 2;
+
+  if (length > 4) {
+    if (stem[length - 1] === 'r')
+      length--;
+    if (stem[length - 1] === 'e')
+      length--;
+    if (stem[length - 1] === 'e')
+      length--;
+    if (stem[length - 1] === stem[length - 2] && /[^\W\d]/.test(stem[length - 1]))
+      length--;
+  }
+
+  return stem.slice(0, length);
+}
+
 export function complex(word) {
-  // WIP
-  return word;
+  let length = word.length,
+      stem = word;
+
+  if (length > 5 && stem[length - 1] === 'x') {
+    if (stem[length - 3] === 'a' &&
+        stem[length - 2] === 'u' &&
+        stem[length - 4] !== 'e') {
+      stem = replaceAt(stem, length - 2, 'l');
+    }
+    length--;
+  }
+
+  if (length > 3 && stem[length - 1] === 'x')
+    length--;
+
+  if (length > 3 && stem[length - 1] === 's')
+    length--;
+
+  if (length > 9 && endsWith(stem, length, 'issement')) {
+    length -= 6;
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 8 && endsWith(stem, length, 'issant')) {
+    length -= 4;
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 6 && endsWith(stem, length, 'ement')) {
+    length -= 4;
+
+    if (length > 3 && endsWith(stem, length, 'ive')) {
+      length--;
+      stem = replaceAt(stem, length - 1, 'f');
+    }
+
+    return normalize(stem, length);
+  }
+
+  if (length > 11 && endsWith(stem, length, 'ficatrice')) {
+    length -= 5;
+    stem = replaceAt(stem, length - 2, 'e');
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 10 && endsWith(stem, length, 'ficateur')) {
+    length -= 4;
+    stem = replaceAt(stem, length - 2, 'e');
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 9 && endsWith(stem, length, 'catrice')) {
+    length -= 3;
+    stem = replaceAt(stem, length - 4, 'q');
+    stem = replaceAt(stem, length - 3, 'u');
+    stem = replaceAt(stem, length - 2, 'e');
+    return normalize(stem, length);
+  }
+
+  if (length > 8 && endsWith(stem, length, 'cateur')) {
+    length -= 2;
+    stem = replaceAt(stem, length - 4, 'q');
+    stem = replaceAt(stem, length - 3, 'u');
+    stem = replaceAt(stem, length - 2, 'e');
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 8 && endsWith(stem, length, 'atrice')) {
+    length -= 4;
+    stem = replaceAt(stem, length - 2, 'e');
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 7 && endsWith(stem, length, 'ateur')) {
+    length -= 3;
+    stem = replaceAt(stem, length - 2, 'e');
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 6 && endsWith(stem, length, 'trice')) {
+    length--;
+    stem = replaceAt(stem, length - 3, 'e');
+    stem = replaceAt(stem, length - 2, 'u');
+    stem = replaceAt(stem, length - 1, 'r');
+  }
+
+  if (length > 5 && endsWith(stem, length, 'ième'))
+    return normalize(stem, length - 4);
+
+  if (length > 7 && endsWith(stem, length, 'teuse')) {
+    length -= 2;
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 6 && endsWith(stem, length, 'teur')) {
+    length--;
+    stem = replaceAt(stem, length - 1, 'r');
+    return normalize(stem, length);
+  }
+
+  if (length > 5 && endsWith(stem, length, 'euse'))
+    return normalize(stem, length - 2);
+
+  if (length > 8 && endsWith(stem, length, 'ère')) {
+    length--;
+    stem = replaceAt(stem, length - 2, 'e');
+    return normalize(stem, length);
+  }
+
+  if (length > 7 && endsWith(stem, length, 'ive')) {
+    length--;
+    stem = replaceAt(stem, length - 1, 'f');
+    return normalize(stem, length);
+  }
+
+  if (length > 4 &&
+      (endsWith(stem, length, 'folle') ||
+       endsWith(stem, length, 'molle'))) {
+    length -= 2;
+    stem = replaceAt(stem, length - 1, 'u');
+    return normalize(stem, length);
+  }
+
+  if (length > 9 && endsWith(stem, length, 'nnelle'))
+    return normalize(stem, length - 5);
+
+  if (length > 9 && endsWith(stem, length, 'nnel'))
+    return normalize(stem, length - 3);
+
+  if (length > 4 && endsWith(stem, length, 'ète')) {
+    length--;
+    stem = replaceAt(stem, length - 2, 'e');
+  }
+
+  if (length > 8 && endsWith(stem, length, 'ique'))
+      length -= 4;
+
+  if (length > 8 && endsWith(stem, length, 'esse'))
+    return normalize(stem, length - 3);
+
+  if (length > 7 && endsWith(stem, length, 'inage'))
+    return normalize(stem, length - 3);
+
+  if (length > 9 && endsWith(stem, length, 'isation')) {
+    length -= 7;
+    if (length > 5 && endsWith(stem, length, 'ual'))
+      stem = replaceAt(stem, length - 2, 'e');
+    return normalize(stem, length);
+  }
+
+  if (length > 9 && endsWith(stem, length, 'isateur'))
+    return normalize(stem, length - 7);
+
+  if (length > 8 && endsWith(stem, length, 'ation'))
+    return normalize(stem, length - 5);
+
+  if (length > 8 && endsWith(stem, length, 'ition'))
+    return normalize(stem, length - 5);
+
+  return normalize(stem, length);
 }
--- test/stemmers/french/unine.js
+++ test/stemmers/french/unine.js
@@ -4,28 +4,119 @@
  *
  */
 import assert from 'assert';
-import unine, {minimal} from '../../../src/stemmers/french/unine';
+import unine, {minimal, complex} from '../../../src/stemmers/french/unine';
 
 describe('unine', function() {
-  it('default export should be the minimal stemmer.', function() {
-    assert(unine('hiboux'), minimal('hiboux'));
+
+  describe('minimal', function() {
+    it('default export should be the minimal stemmer.', function() {
+      assert(unine('hiboux'), minimal('hiboux'));
+    });
+
+    it('should correctly stem the given words.', function() {
+      const tests = [
+        ['mot', 'mot'],
+        ['chevaux', 'cheval'],
+        ['hiboux', 'hibou'],
+        ['chantés', 'chant'],
+        ['chanter', 'chant'],
+        ['chante', 'chant'],
+        ['baronnes', 'baron'],
+        ['barons', 'baron'],
+        ['baron', 'baron']
+      ];
+
+      tests.forEach(function([word, stem]) {
+        assert.strictEqual(unine(word), stem);
+      });
+    });
   });
 
-  it('should correctly stem the given words.', function() {
-    const tests = [
-      ['mot', 'mot'],
-      ['chevaux', 'cheval'],
-      ['hiboux', 'hibou'],
-      ['chantés', 'chant'],
-      ['chanter', 'chant'],
-      ['chante', 'chant'],
-      ['baronnes', 'baron'],
-      ['barons', 'baron'],
-      ['baron', 'baron']
-    ];
+  describe('complex', function() {
+    it('should correctly stem the given words.', function() {
+      const tests = [
+        ['chevaux', 'cheval'],
+        ['cheval', 'cheval'],
+        ['hiboux', 'hibou'],
+        ['hibou', 'hibou'],
+        ['chantés', 'chant'],
+        ['chanter', 'chant'],
+        ['chante', 'chant'],
+        ['chant', 'chant'],
+        ['baronnes', 'baron'],
+        ['barons', 'baron'],
+        ['baron', 'baron'],
+        ['peaux', 'peau'],
+        ['peau', 'peau'],
+        ['anneaux', 'aneau'],
+        ['anneau', 'aneau'],
+        ['neveux', 'neveu'],
+        ['neveu', 'neveu'],
+        ['affreux', 'afreu'],
+        ['affreuse', 'afreu'],
+        ['investissement', 'investi'],
+        ['investir', 'investi'],
+        ['assourdissant', 'asourdi'],
+        ['assourdir', 'asourdi'],
+        ['pratiquement', 'pratiqu'],
+        ['pratique', 'pratiqu'],
+        ['administrativement', 'administratif'],
+        ['administratif', 'administratif'],
+        ['justificatrice', 'justifi'],
+        ['justificateur', 'justifi'],
+        ['justifier', 'justifi'],
+        ['educatrice', 'eduqu'],
+        ['eduquer', 'eduqu'],
+        ['communicateur', 'comuniqu'],
+        ['communiquer', 'comuniqu'],
+        ['accompagnatrice', 'acompagn'],
+        ['accompagnateur', 'acompagn'],
+        ['administrateur', 'administr'],
+        ['administrer', 'administr'],
+        ['productrice', 'product'],
+        ['producteur', 'product'],
+        ['acheteuse', 'achet'],
+        ['acheteur', 'achet'],
+        ['planteur', 'plant'],
+        ['plante', 'plant'],
+        ['poreuse', 'poreu'],
+        ['poreux', 'poreu'],
+        ['plieuse', 'plieu'],
+        ['bijoutière', 'bijouti'],
+        ['bijoutier', 'bijouti'],
+        ['caissière', 'caisi'],
+        ['caissier', 'caisi'],
+        ['abrasive', 'abrasif'],
+        ['abrasif', 'abrasif'],
+        ['folle', 'fou'],
+        ['fou', 'fou'],
+        ['personnelle', 'person'],
+        ['personne', 'person'],
+        ['complète', 'complet'],
+        ['complet', 'complet'],
+        ['aromatique', 'aromat'],
+        ['faiblesse', 'faibl'],
+        ['faible', 'faibl'],
+        ['patinage', 'patin'],
+        ['patin', 'patin'],
+        ['sonorisation', 'sono'],
+        ['ritualisation', 'rituel'],
+        ['rituel', 'rituel'],
+        ['nomination', 'nomin'],
+        ['disposition', 'dispos'],
+        ['dispose', 'dispos'],
+        ['1234555', '1234555'],
+        ['12333345', '12333345'],
+        ['1234', '1234'],
+        ['abcdeff', 'abcdef'],
+        ['abcccddeef', 'abcdef'],
+        ['créées', 'cre'],
+        ['22hh00', '22h00'],
+      ];
 
-    tests.forEach(function([word, stem]) {
-      assert.strictEqual(unine(word), stem);
+      tests.forEach(function([word, stem]) {
+        assert.strictEqual(complex(word), stem);
+      });
     });
   });
 });
