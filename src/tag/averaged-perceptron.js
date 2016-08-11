d027349bb407602bb9f27b8de1a2aeb113722d95
Yomguithereal <guillaumeplique@gmail.com>
2016-08-12 01:06:03 +0200

    Refactor

--- src/tag/averaged-perceptron.js
+++ src/tag/averaged-perceptron.js
@@ -8,6 +8,8 @@
  * [Reference]: http://spacy.io/blog/part-of-speech-POS-tagger-in-python/
  */
 
+// NOTE: only keep relevant state after training & have a flag
+
 /**
  * Defaults.
  */
@@ -18,12 +20,78 @@ const DEFAULTS = {
 };
 
 /**
+ * Helpers.
+ */
+
+/**
+ * Function taking training sentences and returning
+ *
+ * @param  {array}  sentences - Array of sentences being arrays of (word, tag).
+ * @return {object}           - Found classes & tags.
+ */
+function analyzeSentences(sentences) {
+  const classes = new Set(),
+        counts = {},
+        tags = {};
+
+  // Iterating over sentences
+  for (let i = 0, l = sentences.length; i < l; i++) {
+    const sentence = sentences[i];
+
+    // Iterating over words
+    for (let j = 0, m = sentence.length; j < m; j++) {
+      const [word, tag] = sentence[j];
+      classes.add(tag);
+
+      if (!(word in counts))
+        counts[word] = {};
+      if (!(tag in counts[word]))
+        counts[word][tag] = 0;
+      counts[word][tag]++;
+    }
+  }
+
+  // Adding words to the tag dictionary
+  for (const word in counts) {
+    const tagFrequencies = counts[word];
+
+    // Finding max frequency
+    let max = -Infinity,
+        maxTag = null,
+        sum = 0;
+
+    for (const tag in tagFrequencies) {
+      const count = tagFrequencies[tag];
+
+      if (count > max) {
+        maxTag = tag;
+        max = count;
+      }
+
+      sum += count;
+    }
+
+    // We don't add rare words to the dictionary, just unambiguous ones
+    if (sum >= DEFAULTS.frequencyThreshold &&
+        (max / sum) >= DEFAULTS.ambiguityThreshold)
+      tags[word] = maxTag;
+  }
+
+  return {classes, tags};
+}
+
+/**
  * The AveragedPerceptronTagger class.
  *
  * @constructor
  */
 export default class AveragedPerceptronTagger {
-  constructor() {
+  constructor(options) {
+    options = options || {};
+
+    this.options = {
+      iterations: options.iterations || DEFAULTS.iterations
+    };
 
     // Properties
     this.tags = {};
@@ -37,49 +105,9 @@ export default class AveragedPerceptronTagger {
    * @return {AveragedPerceptronTagger} - Returns itself for chaining.
    */
   train(sentences) {
-    const counts = {};
+    const {classes, tags} = analyzeSentences(sentences);
 
-    // Iterating over sentences
-    for (let i = 0, l = sentences.length; i < l; i++) {
-      const sentence = sentences[i];
-
-      // Iterating over words
-      for (let j = 0, m = sentence.length; j < m; j++) {
-        const [word, tag] = sentence[j];
-        this.classes.add(tag);
-
-        if (!(word in counts))
-          counts[word] = {};
-        if (!(tag in counts[word]))
-          counts[word][tag] = 0;
-        counts[word][tag]++;
-      }
-    }
-
-    // Adding words to the tag dictionary
-    for (const word in counts) {
-      const tagFrequencies = counts[word];
-
-      // Finding max frequency
-      let max = -Infinity,
-          maxTag = null,
-          sum = 0;
-
-      for (const tag in tagFrequencies) {
-        const count = tagFrequencies[tag];
-
-        if (count > max) {
-          maxTag = tag;
-          max = count;
-        }
-
-        sum += count;
-      }
-
-      // We don't add rare words to the dictionary, just unambiguous ones
-      if (sum >= DEFAULTS.frequencyThreshold &&
-          (max / sum) >= DEFAULTS.ambiguityThreshold)
-        this.tags[word] = maxTag;
-    }
+    this.classes = classes;
+    this.tags = tags;
   }
 }
