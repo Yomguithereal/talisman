022b1b88521f2039c4fc3eaee98d30ed7e433fac
Yomguithereal <guillaumeplique@gmail.com>
2016-08-12 00:50:01 +0200

    Tagger correct init

--- test/tag/averaged-perceptron.js
+++ test/tag/averaged-perceptron.js
@@ -2,12 +2,87 @@
  * Talisman parsers/brown tests
  * =============================
  */
-import assert from 'assert';
+import {assert} from 'chai';
+import conll from '../../src/parsers/conll';
 import AveragedPercerptronTagger from '../../src/tag/averaged-perceptron';
+import {loadResource} from '../helpers';
 
-const sentences = [
-  [['today', 'NN'], ['is', 'PRP'], ['a', 'PRP'], ['beautiful', 'JJ'], ['day', 'NN']]
+const excerpt = loadResource('conll2000/excerpt.txt'),
+      sentences = conll(excerpt);
+
+const CLASSES = [
+  'NN',
+  'IN',
+  'DT',
+  'VBZ',
+  'RB',
+  'VBN',
+  'TO',
+  'VB',
+  'JJ',
+  'NNS',
+  'NNP',
+  ',',
+  'CC',
+  'POS',
+  '.',
+  'VBP',
+  'VBG',
+  'PRP$',
+  'CD',
+  '``',
+  '\'\'',
+  'VBD',
+  'EX',
+  'MD',
+  '#',
+  '(',
+  '$',
+  ')',
+  'NNPS',
+  'PRP',
+  'JJS',
+  'WP',
+  'RBR',
+  'JJR',
+  'WDT',
+  'WRB',
+  'RBS',
+  'PDT',
+  'RP',
+  ':'
 ];
+
+const TAGS = {
+  'in': 'IN',
+  'the': 'DT',
+  'is': 'VBZ',
+  'to': 'TO',
+  'for': 'IN',
+  ',': ',',
+  'a': 'DT',
+  'from': 'IN',
+  'and': 'CC',
+  '.': '.',
+  'of': 'IN',
+  'by': 'IN',
+  'rates': 'NNS',
+  '%': 'NN',
+  'their': 'PRP$',
+  '``': '``',
+  'The': 'DT',
+  'are': 'VBP',
+  'on': 'IN',
+  '\'\'': '\'\'',
+  'said': 'VBD',
+  'at': 'IN',
+  'be': 'VB',
+  '$': '$',
+  'Mr.': 'NNP',
+  'was': 'VBD',
+  'it': 'PRP',
+  'U.S.': 'NNP'
+};
 
 describe('averaged-perceptron', function() {
 
@@ -17,8 +92,8 @@ describe('averaged-perceptron', function() {
       const tagger = new AveragedPercerptronTagger();
 
       tagger.train(sentences);
-
-      assert.deepEqual(tagger.classes, new Set(['JJ', 'PRP', 'NN']));
+      assert.sameMembers(Array.from(tagger.classes), CLASSES);
+      assert.deepEqual(tagger.tags, TAGS);
     });
   });
 });
