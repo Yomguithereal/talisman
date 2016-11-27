ca788979e00c417705baaa01c05ebf88eadebef3
Yomguithereal <guillaumeplique@gmail.com>
2016-11-27 18:51:48 +0100

    Testing the blocking clusterer

--- test/clustering/blocking.js
+++ test/clustering/blocking.js
@@ -7,36 +7,22 @@
  */
 import assert from 'assert';
 import blockingClusterer from '../../src/clustering/blocking';
-import {distance as identity} from '../../src/metrics/distance/identity';
 import levenshtein from '../../src/metrics/distance/levenshtein';
 
 const DATA = [
-  'abc',
   'abc',
   'bde',
   'bd',
   'bde',
   'bcde',
   'abcde'
-].map(s => s.split(''));
+];
 
-describe.skip('blocking', function() {
+describe('blocking', function() {
 
   it('should cluster as expected.', function() {
-    const clusters = blockingClusterer({distance: identity, radius: 0}, DATA);
+    const clusters = blockingClusterer({distance: levenshtein, radius: 1, blocks: doc => doc.split('')}, DATA);
 
-    // FAIRE LES TEST D'EQUIVALENCE DANS REFINE
-    console.log(clusters);
-
-    // assert.deepEqual(
-    //   clusters,
-    //   [['a', 'a', 'a'], ['b', 'b', 'b'], ['c', 'c']]
-    // );
-  });
-
-  it('should work with fuzzy cases.', function() {
-    const clusters = blockingClusterer({distance: levenshtein, radius: 1}, DATA);
-
-    console.log(clusters);
+    assert.deepEqual(clusters, [['bde', 'bd', 'bcde'], ['bcde', 'abcde']]);
   });
 });
